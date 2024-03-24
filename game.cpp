06132f7d9ea866e6665304af5d80704a28aac14d
mindoll1101 <mindoll1101@naver.com>
2024-03-24 17:41:35 +0900

    fix: drawSnake와 applePos의 충돌로 인한 오류 수정

--- game.cpp
+++ game.cpp
@@ -32,23 +32,24 @@ int apple_y = 0;
 //snake위치 저장
 int snake_arr[(BOARD_SIZE-2)*(BOARD_SIZE-2)][2] = {x, y}; 
 //뱀의 몸 길이 
-int snake_size = 0;                
+int snake_size = 0;              
 
 //점수 저장
 int score = 0;         
 
 //key입력 저장
 bool key_left = false;                             
-bool key_right = true;
+bool key_right = false;
 bool key_up = false;
 bool key_down = false;
 bool key_esc = false;
+bool key_enter = false;
 
 //game play상태 저장
 bool play = false;                                 
 
 void handleInput() {
-  //key 입력이 뱀의 크기가 2 이상일 때 반대로 이동하지 않도록 함.
+  //뱀의 크기가 2 이상일 때 반대로 이동하지 않도록 함.
   if (console::key(console::K_LEFT) && (!key_right || snake_size == 0)) {
     key_left = console::key(console::K_LEFT);
     key_right = false;
@@ -90,6 +91,10 @@ void handleInput() {
   else if(key_down){
     y++;
   }
+  else{
+    key_right = true;
+  }
+  
 }
 
 void restrictInScreen() {
@@ -104,13 +109,16 @@ void restrictInScreen() {
     y = console::SCREEN_HEIGHT - 1;
 }
 
-void drawSnake() {
+void snakePos() {
   for(int i = snake_size; i > 0; i--){    //뱀의 머리를 따라가는 뱀의 몸 위치
     snake_arr[i][0] = snake_arr[i-1][0];
     snake_arr[i][1] = snake_arr[i-1][1];
   }
   snake_arr[0][0] = x;
   snake_arr[0][1] = y;
+}
+
+void drawSnake() {
   for(int i = 0; i <= snake_size; i++){
     //뱀의 몸을 그린다.
     console::draw(snake_arr[i][0], snake_arr[i][1], SNAKE_STRING);
@@ -143,7 +151,11 @@ void drawApple(){
   if(x == apple_x && y == apple_y){
     snake_size += 1;
     score += 10;
+    snakePos();
     applePos();
+  }
+  else{
+    snakePos();
   }
 
   console::draw(apple_x, apple_y, APPLE_STRING);
@@ -166,7 +178,7 @@ void drawWall() {
 
 void gameOver(){
   //game over시 문구를 출력하고 게임 진행 후 사용된 변수들을 초기화한다.
-  for(int i = 1; i <= snake_size; i++){
+  for(int i = 2; i <= snake_size; i++){
     //뱀의 머리와 몸이 부딪혔을 경우
     if(snake_arr[i][0] == x && snake_arr[i][1] == y){
       play = false;
@@ -174,36 +186,37 @@ void gameOver(){
   }
   //게임 종료 조건(뱀이 맵 밖으로 나간 경우, 뱀의 머리와 몸이 부딪힌 경우, 최대 점수를 달성한 경우)
   if(x >= BOARD_SIZE - 1 || x <= 0 || y >= BOARD_SIZE - 1 || y <= 0 || !play || score >= (BOARD_SIZE-2)*(BOARD_SIZE-2) * 10 - 10){
-
-    if(score == (BOARD_SIZE-2)*(BOARD_SIZE-2) * 10 - 10){
-      console::draw(BOARD_SIZE/2 - 4, BOARD_SIZE/2 - 1, "YOU WIN!");
-      console::draw(BOARD_SIZE/2 - 10, BOARD_SIZE/2, "Play new game (Enter)");
-    }
-    else{
-      console::draw(BOARD_SIZE/2 - 4, BOARD_SIZE/2 - 1, "YOU LOSE!");
-      console::draw(BOARD_SIZE/2 - 8, BOARD_SIZE/2, "Try again? (Enter)");
-    }
-    x = BOARD_SIZE/2;    //변수 초기화
-    y = BOARD_SIZE/2;
-    score = 0;
-    snake_size = 0; 
-    snake_arr[0][0] = x;
-    snake_arr[0][1] = y;
-    applePos();
-    key_left = false;
-    key_right = true;
-    key_up = false;
-    key_down = false;
-
-    console::wait();
     while(true){
       //Enter입력 시 재시작, esc입력 시 종료.
+      if(score == (BOARD_SIZE-2)*(BOARD_SIZE-2) * 10 - 10){
+        console::draw(BOARD_SIZE/2 - 4, BOARD_SIZE/2 - 1, "YOU WIN!");
+        console::draw(BOARD_SIZE/2 - 10, BOARD_SIZE/2, "Play new game (Enter)");
+      }
+      else{
+        console::draw(BOARD_SIZE/2 - 4, BOARD_SIZE/2 - 1, "YOU LOSE!");
+        console::draw(BOARD_SIZE/2 - 8, BOARD_SIZE/2, "Try again? (Enter)");
+      }
       if(console::key(console::K_ENTER)){
+        x = BOARD_SIZE/2;    //변수 초기화
+        y = BOARD_SIZE/2;
+        score = 0;
+        snake_size = 0; 
+        snake_arr[0][0] = x;
+        snake_arr[0][1] = y;
+        applePos();
+        key_left = false;
+        key_right = false;
+        key_up = false;
+        key_down = false;
+        key_enter = true;
         break;
       }
       else if(console::key(console::K_ESC)){
         key_esc = true;
         break;
+      }
+      else{
+        console::wait();
       }
     }
   } 
@@ -215,7 +228,6 @@ void game() {
   // 콘솔 라이브러리를 초기화한다.
   console::init();
 
-  play = true;
   applePos();
 
   while (true) {
@@ -223,25 +235,29 @@ void game() {
     console::clear();
 
     play = true;
-    if(key_esc){
-      break;
-    }
 
+    
     drawWall();     //게임 범위 벽 생성
     handleInput();  //이동 제어
     restrictInScreen(); //최대 범위 밖으로 나가지 않도록 함.
-    drawSnake();    //뱀 생성
     drawApple();            //apple을 random 위치에 생성
+    drawSnake();    //뱀 생성
     printScore(score, BOARD_SIZE);
     gameOver();         //벽에 부딪히거나 몸에 부딪히면 게임 오버.
+    
        
 
     // 화면을 갱신하고 다음 프레임까지 대기한다.
-    for(int i = 0; i < MOVE_DELAY; i++){
-      if(key_esc){
-        break;
+    if(key_esc){
+      break;
+    }
+    else if(key_enter){
+      key_enter = false;
+    }
+    else{
+      for(int i = 0; i < MOVE_DELAY; i++){
+        console::wait();
       }
-      console::wait();
     }
   }
 }
